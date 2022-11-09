22683239056c54cfe9cc9afd2026b92c062a719d
Laura <laura.rodriguez@laplateforme.io>
2022-11-09 17:38:30 +0100

    header

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/marseille.css" />
+    <link rel="stylesheet" type="text/css" href="header/footer/header.css" />
+    <link rel="stylesheet" type="text/css" href="header/footer/footer.css" />
+    <title>Accueil</title>
+</head>
+
+<body>
+    <header>
+        <?php include "header/footer/header.html" ?>
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+        <?php include "header/footer/footer.html" ?>
+    </footer>
+</body>
+
+</html>
